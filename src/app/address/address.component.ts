76787fc28d2e11796eaa4811d31e506291007ba7
Tomaz Majerhold <tomaz.majerhold@arnes.si>
2024-11-20 10:05:29 +0100

    simple

---
+++ src/app/address/address.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-address',
+  imports: [],
+  template: `<p>address works!</p>`  
+})
+export class AddressComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddressComponent } from './address/address.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, AddressComponent],
+  template: `
+    <h1>Hello, {{ title }}</h1>
+    <app-address></app-address>
+    <router-outlet />
+  `,  
 })
 export class AppComponent {
   title = 'ng19';
